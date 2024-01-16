81f9871df4968edd01b6d614537ad7fec0040a55
Davi Santos <davi.santos@liferay.com>
2024-01-16 13:50:35 -0300

    Add TestimonialDTOConverter record

---
+++ src/main/java/journey/miles/api/dto/TestimonialDTOConverter.java
@@ -0,0 +1,14 @@
+package journey.miles.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.web.multipart.MultipartFile;
+
+public record TestimonialDTOConverter(@NotBlank String userName,
+                                      @NotBlank @Size(max = 100, message =
+                                              "The testimonial must have a maximum of {max} " +
+                                                      "characters")
+                                      String testimonial,
+                                      MultipartFile profilePicture) {
+}
+    
