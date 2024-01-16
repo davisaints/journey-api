e20981eb3e6dace9d1e4c56cfdaccf404694a068
Davi Santos <davi.santos@liferay.com>
2024-01-16 13:50:03 -0300

    Add Testimonial class

---
+++ src/main/java/journey/miles/api/model/Testimonial.java
@@ -0,0 +1,28 @@
+        package journey.miles.api.model;
+
+        import jakarta.persistence.*;
+        import lombok.*;
+
+        import java.time.LocalDate;
+
+        @Table(name = "testimonials")
+        @Entity(name = "Testimonial")
+        @Getter
+        @Setter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @EqualsAndHashCode(of = "id")
+        public class Testimonial {
+            @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+            private Long id;
+            private String userName;
+            private String testimonial;
+            @Column(columnDefinition = "TEXT")
+            private String profilePicture64;
+
+            public Testimonial(String userName, String testimonial, String profilePicture64) {
+                this.userName = userName;
+                this.testimonial = testimonial;
+                this.profilePicture64 = profilePicture64;
+            }
+        }
