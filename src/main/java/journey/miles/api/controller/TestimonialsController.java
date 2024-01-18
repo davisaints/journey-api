6cde3e69a67bb2cf0e32ccd926fb034efd34274c
Davi Santos <davi.santos@liferay.com>
2024-01-18 09:14:12 -0300

    Sort by createDate

--- src/main/java/journey/miles/api/controller/TestimonialsController.java
+++ src/main/java/journey/miles/api/controller/TestimonialsController.java
@@ -56,7 +56,7 @@ public class TestimonialsController {
     }
 
     @GetMapping
-    public Page<Testimonial> getAllTestimonials(@PageableDefault(size = 3, sort = {"userName"}) Pageable pagination) {
+    public Page<Testimonial> getAllTestimonials(@PageableDefault(size = 3, sort = {"createDate"}) Pageable pagination) {
         return repository.findAll(pagination);
     }
 
