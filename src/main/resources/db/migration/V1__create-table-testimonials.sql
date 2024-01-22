6dfb643b1a97e4f7998dcb74e102a160d4c53c27
Davi Santos <davi.santos@liferay.com>
2024-01-22 09:12:04 -0300

    change userName varchar to 50

--- src/main/resources/db/migration/V1__create-table-testimonials.sql
+++ src/main/resources/db/migration/V1__create-table-testimonials.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS testimonials (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(100) NOT NULL,
+    user_name VARCHAR(50) NOT NULL,
     testimonial TEXT NOT NULL,
     profile_picture64 LONGTEXT,
     PRIMARY KEY (id)
