38e28483306bb7119e400615bf9cd1d6a6239ca5
Davi Santos <davi.santos@liferay.com>
2024-01-29 18:33:08 -0300

    Add method to handle 404

--- src/main/java/journey/miles/api/infra/GlobalExceptionHandler.java
+++ src/main/java/journey/miles/api/infra/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package journey.miles.api.infra;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -11,6 +12,13 @@ import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleNotFound(EntityNotFoundException e) {   
+        var error = e.getMessage();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<String> handleNotFound(NoSuchElementException e) {
