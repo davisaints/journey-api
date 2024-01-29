b3930be93b3667134003af2285f969c9655bb397
Davi Santos <davi.santos@liferay.com>
2024-01-29 18:26:11 -0300

    Add validation messages

--- src/main/java/journey/miles/api/dto/TestimonialDTOConverter.java
+++ src/main/java/journey/miles/api/dto/TestimonialDTOConverter.java
@@ -7,8 +7,8 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDateTime;
 
 public record TestimonialDTOConverter(
-        @NotBlank String userName,
-        @NotBlank @Size(max = 500, message = "The testimonial must " + "have a maximum of {max} characters")
+        @NotBlank(message = "The userName parameter is mandatory") String userName,
+        @NotBlank(message = "The testimonial parameter is mandatory") @Size(max = 500, message = "The testimonial must " + "have a maximum of {max} characters")
         String testimonial,
         MultipartFile profilePicture,
         Long id,
