8d31e05fa501f5ca916e3c66414480d154594205
Davi Santos <davi.santos@liferay.com>
2024-01-29 18:24:45 -0300

    Add GlobalExceptionHandler class

---
+++ src/main/java/journey/miles/api/infra/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package journey.miles.api.infra;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFound(NoSuchElementException e) {
+        var error = e.getMessage();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNPE(NullPointerException e) {
+        var error = e.getMessage();
+
+        return ResponseEntity.internalServerError().body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleBadRequest(MethodArgumentNotValidException e) {
+        var error = e.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(error.stream().map(ExceptionData::new).toList());
+    }
+
+    private record ExceptionData(String field, String message) {
+        public ExceptionData(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
