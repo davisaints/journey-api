b97d7494ec6065b0088972418575ed463ddf48cb
Davi Santos <davi.santos@liferay.com>
2024-03-07 21:07:29 -0300

    Update nomenclature

--- src/main/java/journey/miles/api/constants/Constants.java
+++ src/main/java/journey/miles/api/constants/Constants.java
@@ -1,5 +1,6 @@
 package journey.miles.api.constants;
 
 public class Constants {
-    public static final String[] imageExtensions = {"jpg", "jpeg", "png", "webp", "svg"};
+    public static final String[] IMAGE_EXTENSIONS = {"jpg", "jpeg", "png", "webp", "svg"};
+
 }
